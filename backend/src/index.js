cccffdd3e85d594313f84130adc55e2d1a705693
ImAust1n <austinmiracle007@gmail.com>
2025-02-28 01:27:16 +0530

    Add Complete Authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,39 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js"
 
 dotenv.config()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT;
 
 const app = express()
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.reponse(200).json({ message: "API Server is Live" });
 })
 
+// Use Routes
+app.use('/api/auth', authRoutes);
+// app.use('/api/disasters', disasterRoutes);
+// app.use('/api/donations', donationRoutes);
+// app.use('/api/ngos', ngoRoutes);
+// app.use('/api/gov', govRoutes);
+// app.use('/api/camp', campRoutes);
+// app.use('/api/inventory', inventoryRoutes);
+
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
 })
