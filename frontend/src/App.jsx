a015b1c2570ba556fbfb288f719a436c6048ad3a
ImAust1n <austinmiracle007@gmail.com>
2025-03-03 11:46:50 +0530

    add remember me functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast';
 import './index.css'
 import Header from './components/Header'
 import Navbar from './components/Navbar.jsx'
+import { Loader } from 'lucide-react'
 
 import HomePage from './pages/HomePage'
 
@@ -35,9 +36,23 @@ import { useNGOStore } from './store/useNGOStore.js'
 import { useGOVStore } from './store/useGOVStore.js'
 
 const App = () => {
-  const { authUser } = useAuthStore();
-  const { authNGO } = useNGOStore();
-  const { authGOV } = useGOVStore();
+  const { authUser, checkAuth: checkAuthUser, isCheckingAuth: isCheckingAuthUser } = useAuthStore();
+  const { authNGO, checkAuth: checkAuthNGO, isCheckingAuth: isCheckingAuthNGO } = useNGOStore();
+  const { authGOV, checkAuth: checkAuthGOV, isCheckingAuth: isCheckingAuthGOV } = useGOVStore();
+
+  useEffect(() => {
+    checkAuthUser();
+    checkAuthNGO();
+    checkAuthGOV();
+  }, [checkAuthUser, checkAuthNGO, checkAuthGOV]);
+
+  if (isCheckingAuthUser || isCheckingAuthNGO || isCheckingAuthGOV) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
 
   return (
     <div>
